fb8ac5231ee20753ccee0cf5c0ed8a24cac3a417
David Bogoslavsky <bogo3333@gmail.com>
2021-10-28 11:39:59 +0300

    skicha linear_reg

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -5,7 +5,7 @@
 float var(float *x, int size) {
     float var = 0;
     for (int i = 1; i <= size; ++i) {
-        var = var + pow(x[i], 2));
+        var = var + pow(x[i], 2);
     }
     var = var / float(size);
     var = var - pow(expectedValue(x, size), 2);
@@ -38,60 +38,49 @@ float pearson(float *x, float *y, int size) {
 
 }
 
-class Point {
-public:
-    float x,y;
-    Point(float x, float y):x(x),y(y){}
-
-
-public float xVal() {
-    return x;
-}
-public float yVal() {
-    return y;
-}
-}
 // we create an array of x's from points
-public float[] from_point_to_x(**points, int size) {
-float[] fromPointToFloat = new float[size];
-for(i = 0; i < size; i++) {
-    float[i] == points[i].xVal;
-}
-return fromPointToFloat;
+static float *from_point_to_x(Point **points, int size)
+{
+	float *fromPointToFloat = new float[size];
+	for (int i = 0; i < size; i++) {
+		fromPointToFloat[i] = points[i]->x;
+	}
+	return fromPointToFloat;
 }
 
 // we create an array of y's from points
-public float[] from_point_to_y(**points, int size) {
-float[] fromPointToFloat = new float[size];
-for(i = 0; i < size; i++) {
-    float[i] == points[i].yVal;
-}
-return fromPointToFloat;
+static float *from_point_to_y(Point **points, int size)
+{
+	float *fromPointToFloat = new float[size];
+	for (int i = 0; i < size; i++) {
+		fromPointToFloat[i] = points[i]->y;
+	}
+	return fromPointToFloat;
 }
 
 //we calculate here the avarages of the x and y of the points
-public float avarage_calculator(float **vals, int size) {
-    int sum;
-    for(i = 0; i < size; i++) {
+static float avarage_calculator(float *vals, int size)
+{
+    float sum;
+    for (int i = 0; i < size; i++) {
         sum += vals[i];
     }
-    floatSize = (float)size;
-    return (sum / floatSize);
+    return (sum / (float)size);
 }
+
 // performs a linear regression and return s the line equation
-Line linear_reg(Point **points, int size) {
-float[] fromPointToX = new float[size];
-float[] fromPointToY = new float[size];
-float yAvarage;
-float xAvarage;
-float varA;
-float varB;
-fromPointToX = from_point_to_x(points, size);
-fromPointToY = from_point_to_y(points, size);
-xAvarage = avarage_calculator(fromPointToX, size);
-yAvarage = avarage_calculator(fromPointToY, size);
-varA = (cov(fromPointToX, fromPointToY, size)) / (var(fromPointToX));
-varB = fromPointToY - (varA * fromPointToX);
+Line linear_reg(Point **points, int size)
+{
+	float *fromPointToX = from_point_to_x(points, size);
+	float *fromPointToY = from_point_to_y(points, size);
+	float xAvarage = avarage_calculator(fromPointToX, size);
+	float yAvarage = avarage_calculator(fromPointToY, size);
+	float varA = (cov(fromPointToX, fromPointToY, size)) / (var(fromPointToX, size));
+	float varB = (yAvarage) - (varA * xAvarage);
+	delete[] fromPointToY;
+	delete[] fromPointToX;
+	Line lineReg(varA, varB);
+	return lineReg;
 }
 
 // returns the deviation between point p and the line equation of the points
